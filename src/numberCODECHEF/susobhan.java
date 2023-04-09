ccacaf03589f80f2924a86c0129f56d86c390ef1
SUSOBHANLAL <susobhanlal2017@gmail.com>
2023-04-10 00:46:20 +0530

    first commit

---
+++ src/numberCODECHEF/susobhan.java
@@ -0,0 +1,20 @@
+package numberCODECHEF;
+import java.util.*;
+public class susobhan {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		int a,b,n;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n= sc.nextInt();
+		
+		for(int j= 0;j<n;j++){
+		    a= sc.nextInt();
+		    System.out.println(a/2);
+		}
+	}
+
+}
